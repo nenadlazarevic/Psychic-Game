51ed6d02323c0b5251ace3a6ac96f1fda0044e93
Laza <postlaza@gmail.com>
2018-09-30 14:51:12 -0700

    day1

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+
+
+
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var wins = 0;
+var losses = 0;
+
+var directionsText = document.getElementById("directions-text");
+    var userChoiceText = document.getElementById("userchoice-text");
+    var computerChoiceText = document.getElementById("computerchoice-text");
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var tiesText = document.getElementById("ties-text");
+
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    if ((userGuess === "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")) {
+        
+         if (userGuess === computerGuess) {
+        wins++;
+      } else if (userGuess !== computerGuess) {
+        losses++
+      }
+    }
+    
+
+
+    directionsText.textContent = "";
+
+    // Display the user and computer guesses, and wins/losses/ties.
+    userChoiceText.textContent = "You chose: " + userGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerGuess;
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
+    tiesText.textContent = "ties: " + ties;
+  };
+
+  document.write('hello');
