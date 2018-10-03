9eeaf088291c25d120e006656e40d1c50a95d67b
Laza <postlaza@gmail.com>
2018-10-03 01:09:20 -0700

    test5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,31 +14,34 @@ var numGuesses = 9;
     var computerChoiceText = document.getElementById("computerchoice-text");
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
-    var userselectedtext = document.getElementById("userselected-text");
-    var guessesleft = document.getElementById("guessesleft-text");
+    var numGuessesText = document.getElementById("numGuesses-text");
+    var userselectedtext = document.getElementById("UserSelected-text");
+   
 
 document.onkeyup = function(event) {
-    var userGuess = event.key;
+    
+  var userGuess = event.key;
 
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if ((userGuess === "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")) {
+     {
         
          if (userGuess === computerGuess) {
         wins++;
         numGuesses = 9;
-				userselected = [];
-      } else if (userGuess !== computerGuess) {
-       numGuesses --;
+        userselected = [];
+      }  else if (userGuess !== computerGuess) {
+       numGuesses--;
        userselected.push(userGuess);
-
-      }
+      } 
 
       if (numGuesses === 0) {
         numGuesses = 9;
         losses++;
         userselected = [];
-      }
+      } 
+      
+
     }
 
    
@@ -50,14 +53,19 @@ document.onkeyup = function(event) {
 
 
     directionsText.textContent = "";
+    
+
 
     // Display the user and computer guesses, and wins/losses/ties.
-    userChoiceText.textContent = "You chose: " + userselected;
+    userChoiceText.textContent = "You chose: " + userGuess;
     computerChoiceText.textContent = "The computer chose: " + computerGuess;
     winsText.textContent = "wins: " + wins;
     lossesText.textContent = "losses: " + losses;
-    userselectedText.textContent = "user selected: " + userselected;
-    GuessesleftText.textContent = "guesses left:" + numGuesses;
+    numGuessesText.textContent = "guesses left: " + numGuesses;
+    userselectedtext.textContent = "you selected: " + userselected;
+    
   };
+
+
 
   
