541af245daec2e499ee7a0b53eefabb0891b4497
Laza <postlaza@gmail.com>
2018-10-04 20:46:13 -0700

    test9

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,25 +11,29 @@ var numGuesses = 9;
 
     var directionsText = document.getElementById("directions-text");
     var userChoiceText = document.getElementById("userchoice-text");
-    // var computerChoiceText = document.getElementById("computerchoice-text");
+    var computerChoiceText = document.getElementById("computerchoice-text");
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
     var numGuessesText = document.getElementById("numGuesses-text");
-    // var userselectedtext = document.getElementById("UserSelected-text");
+    var userselectedtext = document.getElementById("UserSelected-text");
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
    
 
 document.onkeyup = function(event) {
     
   var userGuess = event.key;
-
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  if (userGuess === "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 
      {
         
          if (userGuess === computerGuess) {
         wins++;
-        numGuesses = 9;
         userselected = [];
+        numGuesses = 9;
+        
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log(userselected)
          }
         if (event.keyCode < 65 || event.keyCode > 90) {
           alert("Invalid Entry");
@@ -47,6 +51,7 @@ document.onkeyup = function(event) {
         numGuesses = 9;
         losses++;
         userselected = [];
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
       } 
       
 
@@ -66,10 +71,11 @@ document.onkeyup = function(event) {
 
     // Display the user and computer guesses, and wins/losses/ties.
     userChoiceText.textContent = "You chose: " + userselected;
-    // computerChoiceText.textContent = "The computer chose: " + computerGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerGuess;
     winsText.textContent = "wins: " + wins;
     lossesText.textContent = "losses: " + losses;
     numGuessesText.textContent = "guesses left: " + numGuesses;
+    userselectedtext.textContent = "jfjfjf" + userselected;
     
     
   };
